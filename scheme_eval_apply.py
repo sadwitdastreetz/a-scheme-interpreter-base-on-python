6105eb18165d65fe7c99e5983a63c248eef2ba23
sadwitdastreetz <ddgerm@outlook.com>
2025-02-17 23:37:26 +0800

    Add files via upload

---
+++ scheme_eval_apply.py
@@ -0,0 +1,120 @@
+import sys
+
+from pair import *
+from scheme_utils import *
+from ucb import main, trace
+
+import scheme_forms
+
+##############
+# Eval/Apply #
+##############
+
+
+def scheme_eval(expr, env, _=None):  # Optional third argument is ignored
+    """Evaluate Scheme expression EXPR in Frame ENV.
+
+    >>> expr = read_line('(+ 2 2)')
+    >>> expr
+    Pair('+', Pair(2, Pair(2, nil)))
+    >>> scheme_eval(expr, create_global_frame())
+    4
+    """
+    # Evaluate atoms
+    if scheme_symbolp(expr):
+        return env.lookup(expr)
+    elif self_evaluating(expr):
+        return expr
+
+    # All non-atomic expressions are lists (combinations)
+    if not scheme_listp(expr):
+        raise SchemeError('malformed list: {0}'.format(repl_str(expr)))
+    first, rest = expr.first, expr.rest
+    if scheme_symbolp(first) and first in scheme_forms.SPECIAL_FORMS:
+        return scheme_forms.SPECIAL_FORMS[first](rest, env)
+    else:
+        operator = scheme_eval(first, env)
+        def helper(exprrr):
+            return scheme_eval(exprrr, env)
+        operands = rest.map(helper)
+        return scheme_apply(operator, operands, env)
+
+
+def scheme_apply(procedure, args, env):
+    """Apply Scheme PROCEDURE to argument values ARGS (a Scheme list) in
+    Frame ENV, the current environment."""
+    validate_procedure(procedure)
+    if not isinstance(env, Frame):
+       assert False, "Not a Frame: {}".format(env)
+    if isinstance(procedure, BuiltinProcedure):
+        lst = []
+        args_pointer = args
+        while args_pointer != nil:
+            lst.append(args_pointer.first)
+            args_pointer = args_pointer.rest
+        if procedure.need_env:
+            lst.append(env)
+
+        try:
+            return procedure.py_func(*lst)
+        except TypeError as err:
+            raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
+    elif isinstance(procedure, LambdaProcedure):
+        formals = procedure.formals
+        parent_frame = procedure.env
+        new_Env = parent_frame.make_child_frame(formals, args)
+        return eval_all(procedure.body, new_Env)
+        
+    elif isinstance(procedure, MuProcedure):
+        formals = procedure.formals
+        new_Env = env.make_child_frame(formals, args)
+        return eval_all(procedure.body, new_Env)
+    
+    else:
+        assert False, "Unexpected procedure: {}".format(procedure)
+
+
+def eval_all(expressions, env):
+    """Evaluate each expression in the Scheme list EXPRESSIONS in
+    Frame ENV (the current environment) and return the value of the last.
+
+    >>> eval_all(read_line("(1)"), create_global_frame())
+    1
+    >>> eval_all(read_line("(1 2)"), create_global_frame())
+    2
+    >>> x = eval_all(read_line("((print 1) 2)"), create_global_frame())
+    1
+    >>> x
+    2
+    >>> eval_all(read_line("((define x 2) x)"), create_global_frame())
+    2
+    """
+    if expressions == nil:
+        return None
+    
+    def helper(exprrr):
+        return scheme_eval(exprrr, env)
+    
+    expressions = expressions.map(helper)
+    p = expressions
+    while p.rest != nil:
+        p = p.rest
+    return p.first
+
+class Unevaluated:
+    """An expression and an environment in which it is to be evaluated."""
+
+    def __init__(self, expr, env):
+        """Expression EXPR to be evaluated in Frame ENV."""
+        self.expr = expr
+        self.env = env
+
+
+def complete_apply(procedure, args, env):
+    """Apply procedure to args in env; ensure the result is not an Unevaluated."""
+    validate_procedure(procedure)
+    val = scheme_apply(procedure, args, env)
+    if isinstance(val, Unevaluated):
+        return scheme_eval(val.expr, val.env)
+    else:
+        return val
